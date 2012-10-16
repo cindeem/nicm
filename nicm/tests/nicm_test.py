c8d16d4d978f9bb53fe3592bc4af6ceedcc06a87
CindeeM <cindee@berkeley.edu>
2012-10-16 14:30:29 -0700

    TST: add decorator for skipping test if FSL is not installed

--- nicm/tests/nicm_test.py
+++ nicm/tests/nicm_test.py
@@ -7,7 +7,7 @@ from os.path import abspath, join, dirname
 
 import numpy as np
 
-from unittest import TestCase
+from unittest import TestCase, skipIf, skipUnless
 
 from numpy.testing import (assert_raises, assert_equal,
                            assert_almost_equal)
@@ -17,6 +17,15 @@ from ..nicm import (CenterMass, CSVIO,
 
 
 data_path = abspath(join(dirname(__file__), 'data'))
+
+# function to test for installed FSL
+def fsl_missing():
+    """ looks for FSLDIR in environ"""
+    try:
+        os.environ['FSLDIR']
+        return False
+    except:
+        return True
 
 class TestCenterMass(TestCase):
     testnii = join(data_path, 'B00-100', 'test.nii')
@@ -30,6 +39,7 @@ class TestCenterMass(TestCase):
         assert_equal(center_of_mass.thresh, 20)
         assert_equal(center_of_mass._op, '-c')
 
+    @skipIf(fsl_missing(), 'FSL NOT INSTALLED')
     def test_run(self):
         center_of_mass = CenterMass(self.testnii).run()
         assert_almost_equal(center_of_mass[1], 17.1828, decimal=4)
