c07ffeef6908febe672233512cb1369f83c10526
Caleb Wang <cw@berkeley.edu>
2012-11-22 00:06:29 -0800

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils2.core import setup
+
+setup(
+    name = 'nicm',
+    version = '0.1',
+    author = 'Caleb Wang',
+    author_email = 'cw@berkeley.edu',
+    packages = ['nicm', 'nicm.tests'],
+    scripts = ['scripts/nicm_cmd.py'],
+    license = 'LICENSE.txt',
+    install_requires = ['nibabel', 'nipype']
+)
