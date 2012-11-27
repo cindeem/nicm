3c57360f3bdc9d3e7d9b1ca2453705669ca878e5
Caleb Wang <cw@berkeley.edu>
2012-11-27 10:48:41 -0800

    Bugfix: scripts/nicm_cmd.py runs without errors, no longer writes log file unless specified

--- scripts/nicm_cmd.py
+++ scripts/nicm_cmd.py
@@ -5,47 +5,58 @@ import nicm
 import argparse
 import sys
 
-def main(input, outputfile, writemode, fix, threshold,
+def main(input, outputfile, mode, fix, threshold,
          overwrite = True, use_mm = True):
     """outputs center of mass of a file to a csv file
 
     Usage:
         python nicm.py input output
     """
-    if outputfile == None:
-        outputfile = os.path.join(os.path.split(input)[0], 'data.csv')
-    m = CMAnalyze(outputfile, writemode, use_mm, threshold, overwrite)    
-    m.run(input)
+    cm = CenterMass(input)
+    output = cm.run()
+    print output 
+    if mode != 'q':
+        if outputfile == None:
+            outputfile = os.path.join(os.path.split(input)[0], 'data.csv')
+        m = CMAnalyze(outputfile, mode, use_mm, threshold, overwrite)    
+        m.run(input)
+        print os.path.abspath(outputfile)
     if fix:
         t = CMTransform(input)
         t.fix()
-    print os.path.abspath(outputfile)
-    return os.path.abspath(outputfile)
+    return output 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="""
     This is the description of this command""",
-                                     epilog = "nicm_cmd -f /home/user/dir/B12-234/file.nii
-                                               nicm_cmd -o data.csv /home/user/dir/B12-234/file.nii")
+                                     epilog = """nicm_cmd -f /home/user/dir/B12-234/file.nii
+                                               nicm_cmd -o data.csv /home/user/dir/B12-234/file.nii""")
+
     parser.add_argument('input', help = 'nifti file to take as input')
     parser.add_argument('-o', help = 'log file (csv) to output to')
-    parser.add_argument('-m', choices = ['w', 'a'], default = 'w',
-                        help = 'specify a write mode: w[rite], r[ead], a[ppend]') #specify a write mode
+    parser.add_argument('-m', choices = ['w', 'a', 'q'], default = 'q',
+                        help = """specify a write mode: w[rite], r[ead], a[ppend], q[uiet]
+                        write:
+                            creates a new .csv file as log
+                        append:
+                            creates a new .csv file as log if does not exist
+                            else, appends to existing .csv log
+                        quiet:
+                            do not write a log""") #specify a write mode
     statsoption = parser.add_mutually_exclusive_group(help = 'specify mm/voxel space. defaults to mm space.')
     statsoption.add_argument('-c', action = 'store_true', help = 'use mm space')
     statsoption.add_argument('-C', action = 'store_true', help = 'use voxel space')
 
     parser.add_argument('-f', action = 'store_true', help = 'fix: create a centered copy of the file')
-    parser.add_argument('-s', action = 'store_true', help = 'silences writes. prints center of mass only. This is not implemented.')
     parser.add_argument('--no-overwrite', action = 'store_true', 
                         help = 'abort execution if the specified log (.csv) file exists')
     parser.add_argument('-t', default = 20,
                         help = 'specify a threshold for flagging a'\
                         ' file as off center')
-    if len(sys.argv) < 2:
+    try:
         parser.print_help()
-    else:
+    except:
         args = parser.parse_args()
 
         if args.C:
